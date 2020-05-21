2366b52f4eafa50e10ee46655485eafd0de68ee2
Ivo Kruijssen <ivo.kruijssengames@gmail.com>
2020-05-21 16:29:22 +0200

    Added interactive media pages and updated some css

---
+++ InteractiveMedia/adobexd.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Portfolivo | Interactive Media | AdobeXd</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
